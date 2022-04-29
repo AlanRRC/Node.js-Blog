1cb092160598c46eaedcbced76ce509104e15b31
Alan Simpson <asimpson@rrc.ca>
2022-04-29 14:06:39 -0500

    Creating server, handle requests

---
+++ server.js
@@ -0,0 +1,46 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
+
+    // set header content type
+    res.setHeader('content-type', 'text/html')
+
+    //  figure out the path the user wants
+    let path = './views/'
+    switch (req.url) {
+        case '/':
+            path += 'index.html'
+            res.statusCode = 200
+            break
+        case '/about':
+            path += 'about.html'
+            res.statusCode = 200
+            break
+        case '/about-me':
+            res.statusCode = 301
+            res.setHeader('Location', '/about')
+            res.end()
+            break
+        default:
+            path += '404.html'
+            res.statusCode = 404
+            break
+    }
+
+    //  send an html file
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+
+})
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for request on port 3000')
+})

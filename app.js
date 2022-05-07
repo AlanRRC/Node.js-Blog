1c95ff7e148314a24a7ea06a4cb9e5d89e81539a
Alan Simpson <asimpson@rrc.ca>
2022-05-07 09:46:46 -0500

    add mongo connection

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 const express = require('express')
+const morgan = require('morgan')
+
 
 //  express app
 const app = express()
 
+//  connect to mongodb
+const dbURI = 'mongodb+srv://netninja:bluejays@nodetuts.j4g9g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+//  register view engine
+app.set('view engine', 'ejs')
+
 //  listen for requests
 app.listen(3000)
 
+app.use(express.static('public'))
+
+app.use(morgan('dev'))
+
 app.get('/', (req, res) => {
-    // res.send('<p>Home page</p>')
-    res.sendFile('./views/index.html', { root: __dirname })
+    const blogs = [
+        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    ];
+    res.render('index', { title: 'Home', blogs })
 })
+
 
 app.get('/about', (req, res) => {
     //res.send('<p>about page</p>')
-    res.sendFile('./views/about.html', { root: __dirname })
+    res.render('about', { title: 'About' })
 })
 
-// redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about')
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create' });
 })
 
 // 404 page, must go at the bottom
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
+    res.status(404).render('404', { title: '404' })
 })
